47eee5af13959abf6635cc000cc66627244d1ac6
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-22 21:29:19 +0530

    first commit

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+
+const BlogPage = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Link to="/">Home</Link>
+        </>
+    )
+}
+
+export default BlogPage
