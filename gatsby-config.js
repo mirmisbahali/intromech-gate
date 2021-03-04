b059eb2babf9122edba8594e167c30271b6f4fec
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-03-04 22:56:02 +0530

    todo: fix errors when fetching multiple papers

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,124 +35,57 @@ module.exports = {
             type: "AEPaper",
             collection: 'AEPaper/gate2020/AE',
             map: doc => {
-              const data = {}
-              for (const i in doc) {
-                doc[i]['que_type'] = doc[i]['Que.Type']
-                doc[i]['q_no'] = doc[i]['Q.No']
-                doc[i]['sec_name'] = doc[i]['Sec. Name']
-
-                delete doc[i]['Que.Type']
-                delete doc[i]['Q.No']
-                delete doc[i]['Sec. Name']
-
-                Object.assign(data, doc[i])
-              }
-              
-              return data
+              return modifyData(doc)
             }
           },
           {
             type: "CE1Paper",
             collection: 'CE1Paper/gate2020/CE',
             map: doc => {
-              const data = {}
-              for (const i in doc) {
-                doc[i]['que_type'] = doc[i]['Que.Type']
-                doc[i]['q_no'] = doc[i]['Q.No']
-                doc[i]['sec_name'] = doc[i]['Sec. Name']
-
-                delete doc[i]['Que.Type']
-                delete doc[i]['Q.No']
-                delete doc[i]['Sec. Name']
-
-                Object.assign(data, doc[i])
-              }
-              
-              return data
+              return modifyData(doc)
             }
           },
           {
             type: "CE2Paper",
             collection: 'CE2Paper/gate2020/CE',
             map: doc => {
-              const data = {}
-              for (const i in doc) {
-                doc[i]['que_type'] = doc[i]['Que.Type']
-                doc[i]['q_no'] = doc[i]['Q.No']
-                doc[i]['sec_name'] = doc[i]['Sec. Name']
-
-                delete doc[i]['Que.Type']
-                delete doc[i]['Q.No']
-                delete doc[i]['Sec. Name']
-
-                Object.assign(data, doc[i])
-              }
-              
-              return data
+              return modifyData(doc)
             }
           },
           {
             type: "CSPaper",
             collection: 'CSPaper/gate2020/CS',
             map: doc => {
-              const data = {}
-              for (const i in doc) {
-                doc[i]['que_type'] = doc[i]['Que.Type']
-                doc[i]['q_no'] = doc[i]['Q.No']
-                doc[i]['sec_name'] = doc[i]['Sec. Name']
-
-                delete doc[i]['Que.Type']
-                delete doc[i]['Q.No']
-                delete doc[i]['Sec. Name']
-
-                Object.assign(data, doc[i])
-              }
-              
-              return data
+              return modifyData(doc)
             }
           },
           {
             type: "ECPaper",
             collection: 'ECPaper/gate2020/EC',
             map: doc => {
-              const data = {}
-              for (const i in doc) {
-                doc[i]['que_type'] = doc[i]['Que.Type']
-                doc[i]['q_no'] = doc[i]['Q.No']
-                doc[i]['sec_name'] = doc[i]['Sec. Name']
-
-                delete doc[i]['Que.Type']
-                delete doc[i]['Q.No']
-                delete doc[i]['Sec. Name']
-
-                Object.assign(data, doc[i])
-              }
-              
-              return data
+              return modifyData(doc)
             }
           },
-          {
-            type: "EEPaper",
-            collection: 'EEPaper/gate2020/EE',
-            map: doc => {
-              const data = {}
-              for (const i in doc) {
-                doc[i]['que_type'] = doc[i]['Que.Type']
-                doc[i]['q_no'] = doc[i]['Q.No']
-                doc[i]['sec_name'] = doc[i]['Sec. Name']
-
-                delete doc[i]['Que.Type']
-                delete doc[i]['Q.No']
-                delete doc[i]['Sec. Name']
-
-                Object.assign(data, doc[i])
-              }
-              
-              return data
-            }
-          },
+          
         ]
       }
     }
   ],
 };
+
+
+function modifyData(doc) {
+  const data = {}
+  for (const i in doc) {
+    doc[i]['que_type'] = doc[i]['Que.Type']
+    doc[i]['q_no'] = doc[i]['Q.No']
+    doc[i]['sec_name'] = doc[i]['Sec. Name']
+
+    delete doc[i]['Que.Type']
+    delete doc[i]['Q.No']
+    delete doc[i]['Sec. Name']
+
+    Object.assign(data, doc[i])
+  }
+  return data
+}
